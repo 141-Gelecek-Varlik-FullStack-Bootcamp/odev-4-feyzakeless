435e4e1e01a4305ff6c6e7ba75018266c9e325bc
feyzakeless <feyzakeles@gmail.com>
2021-12-18 02:44:42 +0300

    The first version of the project was prepared

---
+++ Pharmacy/Pharmacy.API/Infrastructure/MappingProfile.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Pharmacy.Model.ModelMedicine;
+using Pharmacy.Model.ModelPrescription;
+using Pharmacy.Model.ModelUser;
+
+namespace Pharmacy.API.Infrastructure
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //Kullanici Mapping
+            CreateMap<UserViewModel, Pharmacy.DB.Entities.User>();
+            CreateMap<Pharmacy.DB.Entities.User, UserViewModel>();
+
+            //İlac Mapping
+
+            CreateMap<MedicineViewModel, Pharmacy.DB.Entities.Medicine>();
+            CreateMap<Pharmacy.DB.Entities.Medicine, MedicineViewModel>();
+
+            CreateMap<InsertMedicineViewModel, Pharmacy.DB.Entities.Medicine>();
+            CreateMap<Pharmacy.DB.Entities.Medicine, InsertMedicineViewModel>();
+
+            CreateMap<ListMedicineViewModel, Pharmacy.DB.Entities.Medicine>();
+            CreateMap<Pharmacy.DB.Entities.Medicine, ListMedicineViewModel>();
+
+            CreateMap<UptadeMedicineViewModel, Pharmacy.DB.Entities.Medicine>();
+            CreateMap<Pharmacy.DB.Entities.Medicine, UptadeMedicineViewModel>();
+
+            CreateMap<DeleteMedicineViewModel, Pharmacy.DB.Entities.Medicine>();
+            CreateMap<Pharmacy.DB.Entities.Medicine, DeleteMedicineViewModel>();
+
+            //Recete Mapping
+            CreateMap<PrescriptionViewModel, Pharmacy.DB.Entities.Prescription>();
+            CreateMap<Pharmacy.DB.Entities.Prescription, PrescriptionViewModel>();
+
+        }
+    }
+}
